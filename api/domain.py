3852bcddc4ad7830686bf083f0680a0d219b9d17
buyfakett <buyfakett@vip.qq.com>
2023-11-18 22:24:36 +0800

    :sparkles: feat: 完成域名接口

--- api/domain.py
+++ api/domain.py
@@ -1,12 +1,10 @@
 import logging
 
-from fastapi import APIRouter, Form, File, UploadFile, Request
+from fastapi import APIRouter
 from pydantic import BaseModel
-import os, json
 from models.first_domain import First_domain
 from .base import resp_200, resp_400
-from typing import Union, List
-from fastapi.encoders import jsonable_encoder
+from typing import List
 
 domain = APIRouter()
 
@@ -25,7 +23,6 @@ class DomainModelList(BaseModel):
 
 @domain.get('/list', response_model=List[DomainModelList])
 async def get_domain():
-    global domains
     try:
         domains = await First_domain.filter(is_delete=False).order_by('id')
     except Exception as e:
@@ -64,11 +61,53 @@ async def add_domain(item: AddDomainModel):
     return resp_200(message='插入成功')
 
 
-@domain.delete('/delete')
-async def delete_domain():
-    return {}
+@domain.delete('/delete/{domain_id}')
+async def delete_domain(domain_id: int):
+    try:
+        await First_domain.get(id=domain_id)
+    except Exception as e:
+        logging.error(f"Error fetching domains: {e}")
+        return resp_400(message='没有这条数据')
+    try:
+        await First_domain.filter(id=domain_id).delete()
+    except Exception as e:
+        logging.error(f"Error fetching domains: {e}")
+        return resp_400(message='删除错误')
+    return resp_200(message='删除成功')
+
+
+class EditDomainModel(BaseModel):
+    id: int
+    domain: str
+    domain_manufacturer: str
+    domain_account_key: str
+    domain_account_secret: str
+    is_delete: bool = False
 
 
 @domain.post('/edit')
-async def edit_domain():
-    return {}
+async def edit_domain(item: EditDomainModel):
+    data = {
+        "id": item.id,
+        "domain": item.domain,
+        "domain_manufacturer": item.domain_manufacturer,
+        "domain_account_key": item.domain_account_key,
+        "domain_account_secret": item.domain_account_secret,
+        "is_delete": item.is_delete,
+    }
+    try:
+        old_data = await First_domain.get(id=data['id'])
+    except Exception as e:
+        logging.error(f"Error fetching domains: {e}")
+        return resp_400(message='没有查到该条数据')
+    old_data.domain = data['domain']
+    old_data.domain_manufacturer = data['domain_manufacturer']
+    old_data.domain_account_key = data['domain_account_key']
+    old_data.domain_account_secret = data['domain_account_secret']
+    old_data.is_delete = data['is_delete']
+    try:
+        await old_data.save()
+    except Exception as e:
+        logging.error(f"Error fetching domains: {e}")
+        return resp_400(message='修改错误')
+    return resp_200(message='修改成功')
