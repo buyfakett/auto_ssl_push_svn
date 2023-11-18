62b11bcc8cb5ab963629091dc1a83ae3c5458f23
buyfakett <buyfakett@vip.qq.com>
2023-11-18 15:58:05 +0800

    :sparkles: feat: 新增插入

--- api/domain.py
+++ api/domain.py
@@ -2,15 +2,16 @@ import logging
 
 from fastapi import APIRouter, Form, File, UploadFile, Request
 from pydantic import BaseModel
-import os
+import os, json
 from models.first_domain import First_domain
 from .base import resp_200, resp_400
 from typing import Union, List
+from fastapi.encoders import jsonable_encoder
 
 domain = APIRouter()
 
 
-class DomainModel(BaseModel):
+class DomainModelList(BaseModel):
     id: int
     domain: str
     domain_manufacturer: str
@@ -22,7 +23,7 @@ class DomainModel(BaseModel):
         from_attributes = True
 
 
-@domain.get('/list', response_model=List[DomainModel])
+@domain.get('/list', response_model=List[DomainModelList])
 async def get_domain():
     global domains
     try:
@@ -30,15 +31,37 @@ async def get_domain():
     except Exception as e:
         # 处理异常，可以打印或记录错误信息
         logging.error(f"Error fetching domains: {e}")
-        return resp_400(message='Internal Server Error')
+        return resp_400(message='查询错误')
     # 将数据库模型转换为响应模型
-    response_data = [DomainModel.from_orm(domain).dict() for domain in domains]
+    response_data = [DomainModelList.from_orm(domain).dict() for domain in domains]
     return resp_200(data=response_data)
 
 
+class AddDomainModel(BaseModel):
+    domain: str
+    domain_manufacturer: str
+    domain_account_key: str
+    domain_account_secret: str
+    is_delete: bool = False
+
+
 @domain.put('/add')
-async def add_domain():
-    return {}
+async def add_domain(item: AddDomainModel):
+    data = {
+        "domain": item.domain,
+        "domain_manufacturer": item.domain_manufacturer,
+        "domain_account_key": item.domain_account_key,
+        "domain_account_secret": item.domain_account_secret,
+        "is_delete": item.is_delete,
+    }
+    if data['domain_manufacturer'] != 'ali':
+        return resp_400(401, '暂时不支持该厂商域名')
+    try:
+        await First_domain.create(**data)
+    except Exception as e:
+        logging.error(f"Error fetching domains: {e}")
+        return resp_400(message='插入错误')
+    return resp_200(message='插入成功')
 
 
 @domain.delete('/delete')
