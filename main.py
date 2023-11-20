ddf5b15abd9cd4d9b37fba1e61c64dc5bb5b7a81
buyfakett <buyfakett@vip.qq.com>
2023-11-20 11:33:48 +0800

    :bug: fix: 修改swagger路径和描述2

--- main.py
+++ main.py
@@ -14,40 +14,32 @@ from api.domain import domain
 from api.server import server
 from api.ssl import ssl
 
-app = FastAPI()
+app = FastAPI(
+    openapi_url="/api/openapi.json",
+    docs_url="/api/docs",
+    redoc_url="/api/redoc",
+    title="auto_ssl_push_svn",
+    version="0.1.0",
+    description="自动获取ssl证书，并推送到svn",
+    servers=[
+        {
+            "description": "test",
+            "url": "https://ssl.nmg.buyfakett.top/api",
+        }
+    ],
+    contact={
+        "name": "buyfakett",
+        "url": "https://github.com/buyfakett",
+        "email": "buyfakett@vip.qq.com",
+    }
+)
+
 register_tortoise(
     app=app,
     config=TORTOISE_ORM,
     generate_schemas=True,  # 如果数据库为空，自动生成对应表单，生产环境不能开
     add_exception_handlers=True,  # 调试消息，生产环境不能开
 )
-
-
-def custom_openapi():
-    if app.openapi_schema:
-        return app.openapi_schema
-    openapi_schema = get_openapi(
-        title="auto_ssl_push_svn",
-        version="0.1.0",
-        description="自动获取ssl证书，并推送到svn",
-        routes=app.routes,
-    )
-    app.openapi_schema = openapi_schema
-    return app.openapi_schema
-
-
-app.openapi = custom_openapi
-
-
-@app.get("/api/docs")
-async def get_documentation():
-    return get_swagger_ui_html(openapi_url="/openapi.json", title="auto_ssl_push_svn")
-
-
-@app.get("/api/openapi.json")
-async def get_open_api_endpoint():
-    return app.openapi()
-
 
 app.include_router(domain, prefix='/api/domain', tags=['域名'])
 app.include_router(server, prefix='/api/server', tags=['服务器'])
