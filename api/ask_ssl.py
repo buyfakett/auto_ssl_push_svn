a75987f46727677d909f3c1ac57b393fed7c0aab
buyfakett <buyfakett@vip.qq.com>
2023-12-28 19:45:07 +0800

    :memo: feat: 新增刷新证书2

--- api/ask_ssl.py
+++ api/ask_ssl.py
@@ -5,7 +5,7 @@ import logging
 import os
 
 from api.base import resp_400
-from tt_util.exec_shell import exec_shell
+from tt_util.exec_shell import exec_shell, check_file
 from tt_util.ssh_util import SSHClient
 from tt_util.svn_util import SVNClient
 from tt_util.yaml_util import read_yaml
@@ -23,7 +23,8 @@ class SslFunction(object):
         self.server_host = None
         self.server_password = None
 
-    def ask_ssl(self, aliyun_access_key: str, aliyun_access_secret: str, domain: str, server_host: str, server_password: str):
+    def ask_ssl(self, aliyun_access_key: str, aliyun_access_secret: str, domain: str, server_host: str,
+                server_password: str):
         """
         获取ssl证书
         :param aliyun_access_key:       阿里云access_key
@@ -32,20 +33,9 @@ class SslFunction(object):
         :param server_host:             申请证书服务器的ip
         :param server_password:         申请证书服务器的密码
         """
-        # 上传配置文件
         with open('./temp/' + 'credentials.ini', encoding="utf-8", mode="a") as f:
             f.write(f'dns_aliyun_access_key = {aliyun_access_key}\n')
             f.write(f'dns_aliyun_access_key_secret = {aliyun_access_secret}')
-        ssh = SSHClient(host=server_host, password=server_password)
-        try:
-            ssh.execute_command('mkdir /auto_ssl_push_svn')
-            ssh.upload_file(os.getcwd() + '/temp/credentials.ini', '/auto_ssl_push_svn/credentials.ini')
-        except Exception as e:
-            logging.error(e)
-            return resp_400(message='上传配置文件失败')
-        os.remove(os.getcwd() + '/temp/credentials.ini')
-        logging.info('上传配置文件成功')
-        # 申请证书成功
         with open('./temp/' + 'setup.sh', encoding="utf-8", mode="a") as f:
             f.write('docker run -id --rm \\\n')
             f.write('--name certbot \\\n')
@@ -59,13 +49,22 @@ class SslFunction(object):
             f.write('--agree-tos \\\n')
             f.write('--preferred-challenges dns \\\n')
             f.write("--manual-cleanup-hook 'aliyun-dns clean'")
-        try:
-            ssh.upload_and_execute_script(os.getcwd() + '/temp/setup.sh', '/auto_ssl_push_svn/setup.sh')
-        except Exception as e:
-            logging.error(e)
-            return resp_400(message='上传/执行失败')
-        ssh.close()
+        # 复制配置文件到运行目录
+        exec_shell('mkdir /auto_ssl_push_svn')
+        exec_shell('cp ' + os.getcwd() + '/temp/credentials.ini /auto_ssl_push_svn')
+        exec_shell('cp ' + os.getcwd() + '/temp/setup.sh /auto_ssl_push_svn')
+        os.remove(os.getcwd() + '/temp/credentials.ini')
         os.remove(os.getcwd() + '/temp/setup.sh')
+        # 运行申请证书容器
+        exec_shell('/bin/bash /auto_ssl_push_svn/setup.sh')
+        # 判断是否是泛域名，如果是泛域名生成的文件夹是不带*的
+        if not domain.startswith('*'):
+            if not check_file(f'/etc/letsencrypt/live/{domain}', 'cert*.pem'):
+                resp_400(message='没有成功申请证书')
+        else:
+            if not check_file(f'/etc/letsencrypt/live/{domain[2:]}', 'cert*.pem'):
+                resp_400(message='没有成功申请证书')
+        return True
 
     def upload_svn(self, hostname: str, repo_url: str, domain: str, server_host: str, server_password: str):
         """
@@ -85,40 +84,16 @@ class SslFunction(object):
         logging.info(f'检出日志: {checkout_output}')
         logging.error(f'检出错误: {checkout_error}')
         logging.info(f'检出返回码: {checkout_code}')
-        delete_svn_temp = 'rm -rf ' + os.getcwd() + '/temp/svn'
 
-        ssh = SSHClient(host=server_host, password=server_password)
         # 判断是否是泛域名，如果是泛域名生成的文件夹是不带*的
         if not domain.startswith('*'):
-            if not ssh.wait_for_file(f'/etc/letsencrypt/live/{domain}/cert.pem'):
-                # 在文件存在时执行你的代码
-                exec_shell(delete_svn_temp)
-                return resp_400(message=f'申请{domain}失败')
-            ssh.download_file(f'/etc/letsencrypt/live/{domain}/cert*.pem',
-                              os.getcwd() + f'/temp/svn/{hostname}/ssl/{domain}.cer')
-            ssh.download_file(f'/etc/letsencrypt/live/{domain}/privkey*.pem',
-                              os.getcwd() + f'/temp/svn/{hostname}/ssl/{domain}.key')
-            logging.info(f'申请{domain}成功')
-
-            ssh.execute_command('rm -rf /auto_ssl_push_svn')
-            ssh.close()
-
+            exec_shell(f'cp /etc/letsencrypt/live/{domain}/cert*.pem /temp/svn/{hostname}/ssl/{domain}.cer')
+            exec_shell(f'cp /etc/letsencrypt/live/{domain}/privkey*.pem /temp/svn/{hostname}/ssl/{domain}.key')
             svn_client.add(f"/app/temp/svn/{hostname}/ssl/{domain}.key")
             svn_client.add(f"/app/temp/svn/{hostname}/ssl/{domain}.cer")
         else:
-            if not ssh.wait_for_file(f'/etc/letsencrypt/live/{domain[2:]}/cert.pem'):
-                # 在文件存在时执行你的代码
-                exec_shell(delete_svn_temp)
-                return resp_400(message=f'申请{domain}失败')
-            ssh.download_file(f'/etc/letsencrypt/live/{domain[2:]}/cert*.pem',
-                              os.getcwd() + f'/temp/svn/{hostname}/ssl/_{domain[1:]}.cer')
-            ssh.download_file(f'/etc/letsencrypt/live/{domain[2:]}/privkey*.pem',
-                              os.getcwd() + f'/temp/svn/{hostname}/ssl/_{domain[1:]}.key')
-            logging.info(f'申请{domain}成功')
-
-            ssh.execute_command('rm -rf /auto_ssl_push_svn')
-            ssh.close()
-
+            exec_shell(f'cp /etc/letsencrypt/live/{domain[2:]}/cert*.pem /temp/svn/{hostname}/ssl/_{domain[1:]}.cer')
+            exec_shell(f'cp /etc/letsencrypt/live/{domain[2:]}/privkey*.pem /temp/svn/{hostname}/ssl/_{domain[1:]}.key')
             svn_client.add(f"/app/temp/svn/{hostname}/ssl/_{domain[1:]}.key")
             svn_client.add(f"/app/temp/svn/{hostname}/ssl/_{domain[1:]}.cer")
 
@@ -127,5 +102,5 @@ class SslFunction(object):
         logging.error(f'提交错误: {commit_error}')
         logging.info(f'提交返回码: {commit_code}')
 
-        exec_shell(delete_svn_temp)
+        exec_shell('rm -rf ' + os.getcwd() + '/temp/svn')
         logging.info('上传成功')
