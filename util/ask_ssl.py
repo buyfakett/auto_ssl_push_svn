34429cc1d53c38c2c331a1a7f056853b25210a48
buyfakett <buyfakett@vip.qq.com>
2024-06-12 16:43:43 +0800

    fix: 重构阿里云申请正式，并测试-5

--- util/ask_ssl.py
+++ util/ask_ssl.py
@@ -44,6 +44,7 @@ class SslFunction(object):
         ssh.execute_command('mkdir -p /auto_ssl_push_svn')
         ssh.upload_file(os.getcwd() + '/temp/credentials.ini', '/auto_ssl_push_svn/credentials.ini')
         ssh.upload_file(os.getcwd() + '/scripts/ask_aliyun_ssl.sh', '/auto_ssl_push_svn/setup.sh')
+        ssh.close()
         os.remove(os.getcwd() + '/temp/credentials.ini')
         # 运行申请证书容器
         exec_shell(f'cd /auto_ssl_push_svn && chmod +x setup.sh && ./setup.sh {self.mail} {domain}')
